31f2e03d2d728ff3f20f53245ac231d8c823fe8d
Anderson Marlon <anderson18.marlon@gmail.com>
2024-06-12 00:40:12 -0300

    Update: Visual

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 const ping = require("ping");
 
-const host = "www.google.com"; // Altere para o host que deseja pingar
+const host = "www.google.com.br"; // Altere para o host que deseja pingar
 const interval = 1000; // Intervalo entre pings em milissegundos
-const pingCount = 100; // Número de pings a serem realizados antes de calcular a perda de pacotes
 
 let sentPings = 0;
 let lostPings = 0;
@@ -10,17 +9,19 @@ let lostPings = 0;
 const pingHost = async () => {
   const res = await ping.promise.probe(host);
   sentPings++;
+
   if (!res.alive) {
     lostPings++;
   }
-  console.log(
-    `Ping to ${host}: ${res.alive ? "Alive" : "Dead"} - Time: ${res.time}ms`
-  );
 
-  if (sentPings % pingCount === 0) {
-    const packetLoss = (lostPings / sentPings) * 100;
-    console.log(`Packet Loss: ${packetLoss.toFixed(2)}%`);
-  }
+  const packetLoss = (lostPings / sentPings) * 100;
+  console.log(
+    `${
+      res.alive ? "🟢" : "🔴"
+    } Ping to ${host} | Packet Loss: ${packetLoss.toFixed(2)}% | Ping: ${
+      res.time
+    }ms`
+  );
 };
 
 // Ping the host at regular intervals
